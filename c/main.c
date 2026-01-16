e0492d1f729033985435e208ee86b907bb7fc46f
Noah Schrader <noah.schrader@smarkets.com>
2026-01-16 13:17:08 +0000

    improved parsing

--- c/main.c
+++ c/main.c
@@ -1,4 +1,5 @@
 #include <fcntl.h>
+#include <limits.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -7,8 +8,6 @@
 #include <sys/param.h>
 #include <sys/stat.h>
 #include <unistd.h>
-
-#include <limits.h>
 
 #define TABLE_SIZE 50000
 
@@ -53,16 +52,25 @@ int compare_entries(const void* a, const void* b) {
     return strcmp(arg1.key, arg2.key);
 }
 
+char* parse_station(char* c, char* station) {
+    int index = 0;
+    while (*c != ';') {
+        station[index++] = *c++;
+    }
+    station[index] = '\0';
+    return c + 1;
+}
+
 long parse_temperature(char* temperature) {
     long product = 0;
     int sign = temperature[0] == '-' ? -1 : 1;
 
     int index = temperature[0] == '-' ? 1 : 0;
     product = product * 10 + (temperature[index++] - '0');
-    
+
     index = temperature[index] == '.' ? index + 1 : index;
     product = product * 10 + (temperature[index++] - '0');
-    
+
     bool has_decimal = temperature[index] == '.';
     int digit = has_decimal ? temperature[index + 1] - '0' : 0;
     int multiplier = has_decimal ? 10 : 1;
@@ -76,39 +84,39 @@ int main(int argc, char* argv[]) {
     struct stat fs;
     fstat(fd, &fs);
     char* bytes = mmap(NULL, fs.st_size, PROT_READ, MAP_SHARED, fd, 0);
+    char* end_of_file = bytes + fs.st_size;
     madvise(bytes, fs.st_size, MADV_SEQUENTIAL);
 
-    char line[107];
+    char station[100];
     struct Entry table[TABLE_SIZE];
     for (int i = 0; i < TABLE_SIZE; ++i) {
         table[i].key[0] = '\0';
         table[i].free = true;
     }
 
-    char* line_start = bytes;
-    char* line_end;
-    size_t remaining_bytes = fs.st_size;
-    while ((line_end = memchr(line_start, '\n', remaining_bytes))) {
-        memcpy(line, line_start, line_end - line_start);
-        remaining_bytes -= line_end + 1 - line_start;
-        line_start = line_end + 1;
-        char* station = strtok(line, ";");
-        char* temperature = strtok(NULL, "\n");
-        long f = parse_temperature(temperature);
+    char* c = bytes;
+    while (c != end_of_file) {
+        c = parse_station(c, station);
+        long temperature = parse_temperature(c);
+
+        while (*c != '\n') {
+            ++c;
+        }
+        ++c;
 
         struct Entry* entry = get_entry(table, station);
         if (entry->free) {
-            entry->value.min = 99.9;
+            entry->value.min = 999;
             entry->value.sum = 0.0;
-            entry->value.max = -99.9;
+            entry->value.max = -999;
             entry->value.count = 0;
             strcpy(entry->key, station);
             entry->free = false;
         }
 
-        entry->value.min = MIN(entry->value.min, f);
-        entry->value.sum += f;
-        entry->value.max = MAX(entry->value.max, f);
+        entry->value.min = MIN(entry->value.min, temperature);
+        entry->value.sum += temperature;
+        entry->value.max = MAX(entry->value.max, temperature);
         ++entry->value.count;
     }
 
@@ -116,8 +124,9 @@ int main(int argc, char* argv[]) {
     printf("{");
     for (int i = 0; i < TABLE_SIZE; ++i) {
         if (!table[i].free) {
-            printf("%s=%.1f/%.1f/%.1f, ", table[i].key, table[i].value.min / 10.0,
-                   table[i].value.sum /10.0 / table[i].value.count,
+            printf("%s=%.1f/%.1f/%.1f, ", table[i].key,
+                   table[i].value.min / 10.0,
+                   table[i].value.sum / 10.0 / table[i].value.count,
                    table[i].value.max / 10.0);
         }
     }
