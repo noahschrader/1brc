d703a73a1f30cf1b3d51d1a62faa4f30d38f7399
Noah Schrader <noah.schrader@smarkets.com>
2026-02-02 13:50:53 +0000

    ilp by processing file in chunks

--- c/main.c
+++ c/main.c
@@ -15,6 +15,7 @@
 #include "string.h"
 
 #define TABLE_SIZE 65536
+#define NUM_CHUNKS 4
 
 struct Entry {
     char* key;
@@ -64,6 +65,23 @@ struct Entry* get_entry(struct Entry table[], const char* key, uint8_t length) {
         index = (index + 1) & (TABLE_SIZE - 1);
     }
     return &table[index];
+}
+
+void update_entry(struct Entry* entry, char* station, uint8_t length,
+                  int16_t temperature) {
+    if (entry->key == NULL) {
+        entry->min = 999;
+        entry->sum = 0;
+        entry->max = -999;
+        entry->count = 0;
+        entry->key = station;
+        entry->key_length = length;
+    }
+
+    entry->min = MIN(entry->min, temperature);
+    entry->sum += temperature;
+    entry->max = MAX(entry->max, temperature);
+    ++entry->count;
 }
 
 int compare_entries(const void* a, const void* b) {
@@ -120,6 +138,20 @@ char* parse_temperature(char* c, int16_t* temperature) {
     return c + length + 1;
 }
 
+char* get_next_line(char* c) {
+    __m256i newlines = _mm256_set1_epi8('\n');
+    uint32_t mask = 0;
+    while (!mask) {
+        __m256i chunk = _mm256_loadu_si256((__m256i*)c);
+        __m256i cmp = _mm256_cmpeq_epi8(chunk, newlines);
+        mask = _mm256_movemask_epi8(cmp);
+        c += 32;
+    }
+
+    uint8_t tailing_zeros = __builtin_ctz(mask);
+    return c - 32 + tailing_zeros + 1;
+}
+
 int main() {
     int fd = open("../measurements.txt", O_RDONLY);
     struct stat fs;
@@ -128,33 +160,63 @@ int main() {
     char* end_of_file = bytes + fs.st_size;
     madvise(bytes, fs.st_size, MADV_SEQUENTIAL);
 
-    char* station;
-    uint8_t length;
-    int16_t temperature;
     struct Entry table[TABLE_SIZE];
     for (int i = 0; i < TABLE_SIZE; ++i) {
         table[i].key = NULL;
     }
 
-    char* c = bytes;
-    while (c != end_of_file) {
-        c = parse_station(c, &station, &length);
-        c = parse_temperature(c, &temperature);
+    size_t chunk_size = fs.st_size / NUM_CHUNKS;
+    char* chunk_start[NUM_CHUNKS];
+    char* chunk_end[NUM_CHUNKS];
 
-        struct Entry* entry = get_entry(table, station, length);
-        if (entry->key == NULL) {
-            entry->min = 999;
-            entry->sum = 0.0;
-            entry->max = -999;
-            entry->count = 0;
-            entry->key = station;
-            entry->key_length = length;
+    chunk_start[0] = bytes;
+    for (int i = 1; i < NUM_CHUNKS; ++i) {
+        chunk_start[i] = get_next_line(bytes + i * chunk_size);
+    }
+    for (int i = 0; i < NUM_CHUNKS - 1; ++i) {
+        chunk_end[i] = chunk_start[i + 1];
+    }
+    chunk_end[NUM_CHUNKS - 1] = end_of_file;
+
+    while (chunk_start[0] < chunk_end[0] && chunk_start[1] < chunk_end[1] &&
+           chunk_start[2] < chunk_end[2] && chunk_start[3] < chunk_end[3]) {
+        char *station0, *station1, *station2, *station3;
+        uint8_t length0, length1, length2, length3;
+        int16_t temperature0, temperature1, temperature2, temperature3;
+
+        char* tp0 = parse_station(chunk_start[0], &station0, &length0);
+        char* tp1 = parse_station(chunk_start[1], &station1, &length1);
+        char* tp2 = parse_station(chunk_start[2], &station2, &length2);
+        char* tp3 = parse_station(chunk_start[3], &station3, &length3);
+
+        chunk_start[0] = parse_temperature(tp0, &temperature0);
+        chunk_start[1] = parse_temperature(tp1, &temperature1);
+        chunk_start[2] = parse_temperature(tp2, &temperature2);
+        chunk_start[3] = parse_temperature(tp3, &temperature3);
+
+        struct Entry* e0 = get_entry(table, station0, length0);
+        struct Entry* e1 = get_entry(table, station1, length1);
+        struct Entry* e2 = get_entry(table, station2, length2);
+        struct Entry* e3 = get_entry(table, station3, length3);
+
+        update_entry(e0, station0, length0, temperature0);
+        update_entry(e1, station1, length1, temperature1);
+        update_entry(e2, station2, length2, temperature2);
+        update_entry(e3, station3, length3, temperature3);
+    }
+
+    for (int i = 0; i < NUM_CHUNKS; ++i) {
+        while (chunk_start[i] < chunk_end[i]) {
+            char* station;
+            uint8_t length;
+            int16_t temperature;
+
+            chunk_start[i] = parse_station(chunk_start[i], &station, &length);
+            chunk_start[i] = parse_temperature(chunk_start[i], &temperature);
+
+            struct Entry* entry = get_entry(table, station, length);
+            update_entry(entry, station, length, temperature);
         }
-
-        entry->min = MIN(entry->min, temperature);
-        entry->sum += temperature;
-        entry->max = MAX(entry->max, temperature);
-        ++entry->count;
     }
 
     qsort(table, TABLE_SIZE, sizeof(struct Entry), compare_entries);
